fc0c5183a3c67dd92e2b91c505567629fbd484ba
cc54321 <112970207+cc54321@users.noreply.github.com>
2022-09-27 09:36:14 -0400

    Create space-boxing.js

---
+++ space-boxing.js
@@ -0,0 +1,39 @@
+const promt = require('promt - sync')({signit:true});
+
+let weight = number(prompt("enter you earth weight: "));
+
+let planetnum = number(prompt("enter the number of 
+the planet you will be traveling to. 1-venus, 2-mars, 
+3-jupiter, 4-saturn, 5-uranus, 6-neptune: "));
+
+console.log(weight);
+console.log(planetnum);
+
+if(planet === 1){
+    console.log("you picked venus");
+    console.log(`your weight needs to be: $ {weight * 0.78}`)   
+
+} else if (planet === 2){
+    console.log("you picked mars");
+    console.log(`your weight needs to be: $ {weight * 0.39}`)  
+
+} else if (planet === 3){
+    console.log("you picked jupiter");
+    console.log(`your weight needs to be: $ {weight * 2.65}`)  
+
+} else if (planet === 4){
+    console.log("you picked saturn");
+    console.log(`your weight needs to be: $ {weight * 1.17}`)  
+
+} else if (planet === 5){
+    console.log("you picked uranus");
+    console.log(`your weight needs to be: $ {weight * 1.05}`) 
+     
+} else if (planet === 6){
+    console.log("you picked neptune");
+    console.log(`your weight needs to be: $ {weight * 1.23}`) 
+    
+}else {
+    console.log("invalid entry")
+}
+
