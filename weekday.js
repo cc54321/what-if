454bd3d7fb7e3bf4ee9c68771ba0ef0475c079cc
cc54321 <112970207+cc54321@users.noreply.github.com>
2022-09-27 09:34:45 -0400

    Create weekday.js

---
+++ weekday.js
@@ -0,0 +1,25 @@
+const promt = require('promt - sync')({signit:true});
+
+
+
+let weekday = number(prompt("please enter the number of the weekday:"));
+console.log(weekday);
+
+if (weekday === 1){
+    console.log("today is monday");
+} else if (weekday === 2){
+    console.log("today is tuesday");
+} else if (weekday === 3){
+    console.log("today is wednesday");
+} else if (weekday === 4){
+    console.log("today is thursday");
+} else if (weekday === 5){
+    console.log("today is friday");
+} else if (weekday === 6){
+    console.log("today is saturday");
+} else if (weekday === 7){
+    console.log("today is sunday");
+} else 
+
+    console.log(error);
+}
