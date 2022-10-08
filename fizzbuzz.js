94fe2a2e93e757010af14eab07c4b7aa17ec50d9
cc54321 <112970207+cc54321@users.noreply.github.com>
2022-10-08 15:37:26 -0400

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,4 +1,12 @@
-const promt = require('promt - sync')({signit:true});
+/*fizzbuzz.js
+This is a classic interview question. 
+Write a program that asks the user for a number. If the number is divisible by 3, the program logs "fizz". 
+If the number is divisible by 5, log "buzz". 
+If the number is divisible by 3 and 5, log "fizzbuzz".
+*/
+
+
+const prompt = require('prompt-sync')({signit:true});
 
 let num = Number(prompt("enter a number:"));
 
@@ -13,3 +21,4 @@ console.log("fizbuzz");
 } else if(num % 5 === 0){
     console.log("buzz");
 }
+
